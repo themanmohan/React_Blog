049ca3a97e8563a902f3e724352d9b3780afdee5
themanmohan <gitmail223344@gmail.com>
2022-04-02 23:06:24 +0530

    feat: react app setup

---
+++ src/topbar/topbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './topbar.css'
+
+export default function TopBar() {
+  return (
+    <div className='top'>
+        <div className='topLeft'>l</div>
+        <div className='topCenter'>c</div>
+        <div className='topRight'>r</div>
+    </div>
+  )
+}
